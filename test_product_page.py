6eee8b78ea491914ad6fa3563813aad5cedf7ce1
miktuy <miktuy@mail.ru>
2020-07-08 19:23:43 +0300

    fixed links problems

--- test_product_page.py
+++ test_product_page.py
@@ -7,16 +7,7 @@ from .pages.basket_page import BasketPage
 from .pages.login_page import LoginPage
 
 
-links = ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
-        "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
-        "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
-        "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
-        "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
-        "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
-        "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
-        pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7", marks=pytest.mark.xfail),
-        "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
-        "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"]
+links = ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0"]
 
 
 @pytest.mark.need_review
