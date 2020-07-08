a2c2d0d381f44c3dcfd8a35ea4669b8ce1ce507e
miktuy <miktuy@mail.ru>
2020-07-08 19:15:31 +0300

    fixed import problems

--- test_product_page.py
+++ test_product_page.py
@@ -2,9 +2,9 @@ import time
 
 import pytest
 
-from pages.product_page import BookOrderPage
-from pages.basket_page import BasketPage
-from pages.login_page import LoginPage
+from .pages.product_page import BookOrderPage
+from .pages.basket_page import BasketPage
+from .pages.login_page import LoginPage
 
 
 links = ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
@@ -19,6 +19,18 @@ links = ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?pr
         "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"]
 
 
+@pytest.mark.need_review
+@pytest.mark.parametrize('link', links)
+def test_guest_can_add_product_to_basket(browser, link):
+    page = BookOrderPage(browser, link)
+    page.open()
+    title = page.get_product_title()
+    page.should_be_add_to_basket_btn()
+    page.add_to_basket()
+    page.solve_quiz_and_get_code()
+    page.should_be_at_messages(title)
+
+
 def test_guest_should_see_login_link_on_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = BookOrderPage(browser, link)
@@ -26,6 +38,7 @@ def test_guest_should_see_login_link_on_product_page(browser):
     page.should_be_login_link()
 
 
+@pytest.mark.need_review
 def test_guest_can_go_to_login_page_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = BookOrderPage(browser, link)
@@ -56,6 +69,8 @@ def test_message_disappeared_after_adding_product_to_basket(browser, link):
     page.solve_quiz_and_get_code()
     page.should_be_disappeared_messages()
 
+
+@pytest.mark.need_review
 @pytest.mark.parametrize('link', links)
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser, link):
     page = BookOrderPage(browser, link)
@@ -83,6 +98,7 @@ class TestUserAddToBasketFromProductPage:
         page.open()
         page.should_not_be_at_messages()
 
+    @pytest.mark.need_review
     def test_user_can_add_product_to_basket(self, browser, link):
         page = BookOrderPage(browser, link)
         page.open()
