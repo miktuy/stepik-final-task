d4f113a44885911d1607c178bd5d4eb28ae1512e
miktuy <miktuy@mail.ru>
2020-07-08 13:30:55 +0300

    added login page description

--- test_main_page.py
+++ test_main_page.py
@@ -1,4 +1,5 @@
 from pages.main_page import MainPage
+from pages.login_page import LoginPage
 
 
 def test_guest_can_go_to_login_page(browser):
@@ -6,6 +7,8 @@ def test_guest_can_go_to_login_page(browser):
     main_page = MainPage(browser, link)
     main_page.open()
     main_page.go_to_login_page()
+    # login_pag = LoginPage
+
 
 
 def test_guest_should_see_login_link(browser):
