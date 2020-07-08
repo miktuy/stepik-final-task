31bb9da060d6083eb04ae328f08b96fa25a8c90f
miktuy <miktuy@mail.ru>
2020-07-08 13:36:03 +0300

    reworked test for login page

--- pages/login_page.py
+++ pages/login_page.py
@@ -9,15 +9,12 @@ class LoginPage(BasePage):
         self.should_be_register_form()
 
     def should_be_login_url(self):
-        # реализуйте проверку на корректный url адрес
-        assert "login" in self.browser.current_url
+        assert "login" in self.browser.current_url, "Current url doesn't containt `login` part"
 
     def should_be_login_form(self):
-        # реализуйте проверку, что есть форма логина
         assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), \
             "Login form is not found"
 
     def should_be_register_form(self):
-        # реализуйте проверку, что есть форма регистрации на странице
         assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), \
             "Register form is not found"
--- test_main_page.py
+++ test_main_page.py
@@ -7,8 +7,8 @@ def test_guest_can_go_to_login_page(browser):
     main_page = MainPage(browser, link)
     main_page.open()
     main_page.go_to_login_page()
-    # login_pag = LoginPage
-
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
 
 
 def test_guest_should_see_login_link(browser):
